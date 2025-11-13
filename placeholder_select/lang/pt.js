c2711a9e064d65043ad800603c1bd6ed5d44333a
Josep Rius <josepriusfont@gmail.com>
2025-11-13 10:43:11 +0100

    Add Portuguese language

---
+++ placeholder_select/lang/pt.js
@@ -0,0 +1,5 @@
+CKEDITOR.plugins.setLang('placeholder_select', 'pt', {
+    dropdown_label: 'Inserir marcador',
+    dropdown_title: 'Inserir marcador',
+    dropdown_voiceLabel: 'Inserir marcador'
+});
